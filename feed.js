3af12cec5e2f0c98befdeac9eed7b81b71c78ece
katie zhu <to.kzhu@gmail.com>
2016-12-24 14:36:39 -0600

    feed updates

--- feed.js
+++ feed.js
@@ -10,11 +10,19 @@ let desc = 'A podcast about this Asian American life'
 let author = 'katie zhu and Nicole Zhu'
 let feedLink = 'http://sweetandsour.fm/feed.rss'
 let options = {
-  'image_url': 'http://sweetandsour.fm/assets/img/sweetandsour.png',
+  'language': 'en',
+  'docs': 'https://cyber.harvard.edu/rss/rss.html',
+  'image': 'http://sweetandsour.fm/assets/img/sweetandsour.png',
   'itunes:subtitle': 'A show about this Asian American life',
+  'itunes:author': 'katie zhu and Nicole Zhu',
   'itunes:image': 'http://sweetandsour.fm/assets/img/sweetandsour.png',
   'itunes:summary': 'Sweet and Sour is a podcast exploring different facets of Asian American life.',
   'itunes:category': 'Society & Culture',
+  'itunes:owner': {
+    'itunes:name': 'katie zhu and Nicole Zhu',
+    'itunes:email': 'to.kzhu@gmail.com'
+  },
+  'itunes:explicit': 'clean',
   'copyright': '2016 Sweet and Sour FM',
   custom_namespaces: {
     'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
